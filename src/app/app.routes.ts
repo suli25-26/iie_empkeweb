0e9dafae083b6d78dbeaaf1cc4c666011ec4810f
Erős István <eros@valahol>
2026-01-28 20:45:55 +0100

    Guard működik

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { authGuard } from './shared/auth-guard';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'employee', component: EmployeeComponent },
+    { path: 'employee', component: EmployeeComponent, 
+        canActivate: [authGuard]
+     },
     { path: '', component: HomeComponent },
 ];
